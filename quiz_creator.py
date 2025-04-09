d9f684b67f533f3c9b196d67c61dfb4616ff13cf
izartorius <atreuseinzbelt@gmail.com>
2025-04-09 10:10:34 +0800

    managed the text files using with statement, then added a while loop to constantly ask for user inputs. if statement was also added to provide exit option to the end user

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,12 @@
 #Assign variables for the text files
 quiz_file = "quiz.txt"
 answer_file = "answer.txt"
+
+#Open and manage the files
+with open(quiz_file, "a") as quiz, open(answer_file, "a") as answer:
+    #Loop sequence to constantly ask for user input
+    while True:
+        questions = input("Add a question: ")
+        if questions == "exit":
+            print("Saving inputs.. Exiting..")
+            break
