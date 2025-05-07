dc09ef5fb191368f1f8d3b867124d6965cb19cd4
izartorius <atreuseinzbelt@gmail.com>
2025-05-07 09:52:00 +0800

    Defined functions for reading the text files and assigned a method for stripping all the blank lines in the text file to avoid getting empty lines in getting contents of the text file

--- quiz_reader.py
+++ quiz_reader.py
@@ -1,4 +1,13 @@
 #Define functions for loading the text files
+def load_quiz(questions_file, answers_file):
+    #Open and read the questions file
+    with open(questions_file, 'r') as qf:
+        #Strips all the spaces and blank lines in the text file
+        lines = [line.strip() for line in qf if line.strip() != '']
+    #Open and read the answers file
+    with open(answers_file, 'r') as af:
+        #Strips all the spaces and blank lines in the text file
+        answers = [line.strip() for line in af if line.strip() != '']
 #Remove all spaces in the text file
 #Take every 5 lines from the text file as a one question
 #Define function for running the quiz game
