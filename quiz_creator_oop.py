0795a85c7c68cb05cd73cfaec602c4acea27409b
izartorius <atreuseinzbelt@gmail.com>
2025-05-26 14:08:30 +0800

    added a function that uses the with statement to carefully handle the text files being opened, then constantly asks the end user for questions using while loop and introduces an exit option using if statement.

--- quiz_creator_oop.py
+++ quiz_creator_oop.py
@@ -4,3 +4,18 @@ class QuizManager:
         #Initialize the QuizManager with file paths for storing questions and answers.
         self.quiz_file = quiz_file
         self.answer_file = answer_file
+
+    def run(self):
+        """
+        Main loop to run the quiz creation interface.
+        Continuously asks the user for input until they type 'Exit'.
+        """
+        with open(self.quiz_file, "a") as quiz, open(self.answer_file, "a") as answer:
+            while True:
+                # Get the quiz question from the user
+                question = input("Add a question: ").capitalize()
+
+                # Check for exit condition
+                if question.lower() == "exit":
+                    print("Saving inputs.. Exiting..")
+                    break
